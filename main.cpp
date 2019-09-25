64380d75aca8baf27508dfb00f9e7b2220535cc1
Ty Conway <wtyconway@gmail.com>
2019-09-24 19:59:28 -0500

    Add doubling array stack class

--- main.cpp
+++ main.cpp
@@ -33,7 +33,8 @@ public:
 };
 
 template <class T>
-class ArrayStack {
+class ArrayStack 
+{
     T* stack; // array
     int topIndex = -1;
     int capacity;
@@ -65,7 +66,9 @@ public:
     }
     T pop()
     {
-
+        T popped = stack[topIndex];
+        topIndex -= 1;
+        return popped;
     }
     void push(T e)
     {
@@ -89,6 +92,66 @@ public:
     }
 };
 
+template <class T>
+class DoublingArrayStack : public ArrayStack<T>
+{
+    T* stack;
+    int topIndex = -1;
+    int capacity;
+public:
+    DoublingArrayStack(int initialCapacity)
+    {
+        capacity = initialCapacity;
+        stack = new T[capacity];
+    }
+    ~DoublingArrayStack()
+    {
+        // TODO
+    }
+    bool isEmpty() 
+    {
+        if (topIndex == -1)
+        {
+            return true;
+        }
+        return false;
+    }
+    int size()
+    {
+        return capacity;
+    }
+    T top()
+    {
+        if (isEmpty()) { throw EmptyStack(); }
+        return stack[topIndex];
+    }
+    T pop()
+    {
+        T popped = stack[topIndex];
+        topIndex -= 1;
+        return popped;
+    }
+    void push(T e)
+    {
+        if (topIndex + 1 == capacity)
+        {
+            T* newStack = new T[capacity * 2];
+            for (int index = 0; index < capacity; index++)
+            {
+                newStack[index] = stack[index];
+            }
+            stack = newStack;
+            capacity *= 2;
+            topIndex += 1;
+            stack[topIndex] = e;
+
+            return;
+        }
+        topIndex += 1;
+        stack[topIndex] = e;
+    }
+};
+
 int main() 
 {
     // Request: reduce CPU use (vagrant?)
@@ -101,6 +164,17 @@ int main()
             arrStack.push(i);
             printf("[%i, %i]\n", arrStack.top(), arrStack.size());
         }
+        cout << "Pop: " << arrStack.pop() << endl;
+
+        DoublingArrayStack<double> doubStack(1000);
+        cout << "\nDoublingArrayStack:\n";
+        for (int i = 1; i <= 10000; i++)
+        {
+            doubStack.push(i);
+            printf("[%f, %i]\n", doubStack.top(), doubStack.size());
+        }
+        cout << "Popped: " << doubStack.pop() << endl;
+        cout << doubStack.size() << endl;
     }
     catch(const exception& e)
     {
